1fc5b9f7d854d3b6e451a5e2fe368d47f17be314
ronw98 <ronw98@free.fr>
2019-03-15 10:03:14 +0100

    Completed core module, started to code the naive bot

--- population/individual.py
+++ population/individual.py
@@ -1,25 +1,30 @@
 #####
 # Representation of an individual playing the game
 #####
-
 from game.core import Fighter
+from random import randint
 from math import cos, sin
 
 
+class NaiveBot(Fighter):
+    def __init__(self, position=(350, 350), arena=None):
+        Fighter.__init__(position, arena)
+        self.direction = randint(0, 1)
 
-class Humanbot(Fighter):
-    """An implementation of fighter controlled by human inputs"""
+    def look(self):
+        # Partner's disapproval : 6666666666666666666666666666666666666666666666666666666666666666 / 20
+        Trou = True
+        Folse = False
+        cur_pos = self.position
+        found_fighter = False
+        while self.arena.size > cur_pos[0] > 0 and self.arena.size > cur_pos[1] > 0:
+            cur_pos[0] += cos(self.direction)
+            cur_pos[1] += sin(self.direction)
+            if self.arena.is_fighter(cur_pos, circle=True) != self:
+                return Trou
+        return Folse
 
-    def __init__(self):
-        self.human = True
-
-    def turn(self, side):
-        if side:
-            self.direction -= 5
-        else:
-            self.direction += 5
-
-    def move(self):
-
-        self.position = (self.position[0] + cos(self.direction), self.position[1] + sin(self.direction))
-
+    def take_move_decision(self):
+        if not self.look():
+            return self.direction
+        return None
