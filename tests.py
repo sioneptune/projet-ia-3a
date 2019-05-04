4fd24830f1cdb226a6dac001273501e7e477b377
ronw98 <ronw98@free.fr>
2019-05-04 17:01:00 +0200

    Created tests

---
+++ tests.py
@@ -0,0 +1,34 @@
+import unittest
+from game.core import *
+from math import sqrt
+
+
+class CleverTest(unittest.TestCase):
+
+    def setUp(self):
+        global arena
+        arena = Arena()
+        global clever
+        clever = CleverBot([1, 2, 3], position=(350, 350), direction=0, arena=arena)
+
+    def test_look_one_dir(self):
+        results = clever.look_one_direction(0)
+        self.assertEqual(results[0], 1/100)
+        self.assertEqual(results[1], 1/200)
+        results = clever.look_one_direction(90)
+        self.assertEqual(results[0], 0)
+        self.assertNotEqual(results[1], 0)
+
+    def test_look(self):
+        results = clever.look()
+        expected = [0, 1/350, 0, 2/(arena.size*sqrt(2)),
+                    0, 1/350, 0, 2/(arena.size*sqrt(2)),
+                    0, 1/350, 0, 2/(arena.size*sqrt(2)),
+                    0, 1/350, 0, 2/(arena.size*sqrt(2))]
+        self.assertEqual(len(results), 16)
+        for i in range(0, 16):
+            self.assertAlmostEqual(results[i], expected[i], delta=0.0005)
+
+
+if __name__ == '__main__':
+    unittest.main()
