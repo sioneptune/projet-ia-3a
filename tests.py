6d8cea6aae1721dfcb542b1952a2f3c922c1da87
ronw98 <ronw98@free.fr>
2019-05-04 17:10:37 +0200

    Added tests

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 import unittest
+import os
 from game.core import *
 from math import sqrt
 
@@ -12,6 +13,9 @@ class CleverTest(unittest.TestCase):
         clever = CleverBot([1, 2, 3], position=(350, 350), direction=0, arena=arena)
 
     def test_look_one_dir(self):
+        naive = NaiveBot(position=(600,200), direction=0)
+        arena.add_fighter(naive)
+        clever.position = [500,200]
         results = clever.look_one_direction(0)
         self.assertEqual(results[0], 1/100)
         self.assertEqual(results[1], 1/200)
